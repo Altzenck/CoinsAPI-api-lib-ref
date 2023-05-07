044ee078956281603ecd95b5679cfbd19725e6ca
Altzenck <58193685+Altzenck@users.noreply.github.com>
2023-05-07 16:06:41 -0500

    Add files via upload

---
+++ src/main/java/de/NeonnBukkit/CoinsAPI/API.java
@@ -0,0 +1,27 @@
+package de.NeonnBukkit.CoinsAPI;
+
+import com.sun.jdi.ClassNotPreparedException;
+
+public class CoinsAPI {
+	  public static boolean playerExists(String uuid) {
+		throw new ClassNotPreparedException("This class is for reference only.");
+	    return false;
+	  }
+	  
+	  public static void createPlayer(String uuid) {
+	  }
+	  
+	  public static Integer getCoins(String uuid) {
+		  throw new ClassNotPreparedException("This class is for reference only.");
+		  return 0;
+	  }
+	  
+	  public static void setCoins(String uuid, Integer Coins) {
+	  }
+	  
+	  public static void addCoins(String uuid, Integer Coins) {
+	  }
+	  
+	  public static void removeCoins(String uuid, Integer Coins) {
+	  }
+}
