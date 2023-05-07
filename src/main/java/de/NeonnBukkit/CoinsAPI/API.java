8d0b93a1408b707f426430a5350c47ced406b8f8
Altzenck <58193685+Altzenck@users.noreply.github.com>
2023-05-07 16:53:07 -0500

    Update API.java

--- src/main/java/de/NeonnBukkit/CoinsAPI/API.java
+++ src/main/java/de/NeonnBukkit/CoinsAPI/API.java
@@ -1,11 +1,10 @@
-package de.NeonnBukkit.CoinsAPI;
+package main.java.de.NeonnBukkit.CoinsAPI.API;
 
 import com.sun.jdi.ClassNotPreparedException;
 
 public class CoinsAPI {
 	  public static boolean playerExists(String uuid) {
 		throw new ClassNotPreparedException("This class is for reference only.");
-	    return false;
 	  }
 	  
 	  public static void createPlayer(String uuid) {
@@ -13,7 +12,6 @@ public class CoinsAPI {
 	  
 	  public static Integer getCoins(String uuid) {
 		  throw new ClassNotPreparedException("This class is for reference only.");
-		  return 0;
 	  }
 	  
 	  public static void setCoins(String uuid, Integer Coins) {
@@ -25,3 +23,4 @@ public class CoinsAPI {
 	  public static void removeCoins(String uuid, Integer Coins) {
 	  }
 }
+
